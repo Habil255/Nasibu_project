1d6be0c05ed48749dd06212b00a4f2b2375ebdac
Habil <raphelhabil09@gmail.com>
2021-06-23 13:12:44 +0300

    http_to_https_changes

--- resources/views/pages/dashlinks.blade.php
+++ resources/views/pages/dashlinks.blade.php
@@ -6,7 +6,7 @@
   <title> Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback')}}">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{secure_asset('plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
@@ -30,38 +30,38 @@
 
   @yield('content')
 
-  <script src="plugins/jquery/jquery.min.js"></script>
+  <script src="{{secure_asset('plugins/jquery/jquery.min.js')}}"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="'plugins/jquery-ui/jquery-ui.min.js'"></script>
+<script src="{{secure_asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{secure_asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- ChartJS -->
-<script src="plugins/chart.js/Chart.min.js"></script>
+<script src="{{secure_asset('plugins/chart.js/Chart.min.js')}}"></script>
 <!-- Sparkline -->
-<script src="{{asset('plugins/sparklines/sparkline.js')}}"></script>
+<script src="{{secure_asset('plugins/sparklines/sparkline.js')}}"></script>
 <!-- JQVMap -->
-<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{secure_asset('plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{secure_asset('plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
 <!-- jQuery Knob Chart -->
-<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="{{secure_asset('plugins/jquery-knob/jquery.knob.min.js')}}"></script>
 <!-- daterangepicker -->
-<script src="{{asset('plugins/moment/moment.min.js')}}"></script>
-<script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{secure_asset('plugins/moment/moment.min.js')}}"></script>
+<script src="{{secure_asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="{{asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<script src="{{secure_asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
 <!-- Summernote -->
-<script src="{{asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
+<script src="{{secure_asset('plugins/summernote/summernote-bs4.min.js')}}"></script>
 <!-- overlayScrollbars -->
-<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="{{secure_asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset('dist/js/adminlte.js')}}"></script>
+<script src="{{secure_asset('dist/js/adminlte.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('dist/js/demo.js')}}"></script>
+<script src="{{secure_asset('dist/js/demo.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('dist/js/pages/dashboard.js')}}"></script>
+<script src="{{secure_asset('dist/js/pages/dashboard.js')}}"></script>
 </body>
 </html>
--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -7,35 +7,35 @@
 
   <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700|Oswald:400,700" rel="stylesheet">
 
-  <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('fonts/icomoon/style.css')}}">
 
-  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/jquery.fancybox.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-  <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
-<link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
-  <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{secure_asset('css/jquery.fancybox.min.css')}}">
+<link rel="stylesheet" href="{{secure_asset('css/owl.carousel.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('css/owl.theme.default.min.css')}}">
+<link rel="stylesheet" href="{{secure_asset('fonts/flaticon/font/flaticon.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('css/aos.css')}}">
 
   <!-- MAIN CSS -->
-  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('css/style.css')}}">
 
 </head>
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
     @yield('content')
 </div>
 
-<script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
-<script src="{{asset('js/popper.min.js')}}"></script>
-<script src="{{asset('js/bootstrap.min.js')}}"></script>
-<script src="{{asset('js/owl.carousel.min.js')}}"></script>
-<script src="{{asset('js/jquery.sticky.js')}}"></script>
-<script src="{{asset('js/jquery.waypoints.min.js')}}"></script>
-<script src="{{asset('js/jquery.animateNumber.min.js')}}"></script>
-<script src="{{asset('js/jquery.fancybox.min.js')}}"></script>
-<script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
-<script src="{{asset('js/aos.js')}}"></script>
+<script src="{{secure_asset('js/jquery-3.3.1.min.js')}}"></script>
+<script src="{{secure_asset('js/popper.min.js')}}"></script>
+<script src="{{secure_asset('js/bootstrap.min.js')}}"></script>
+<script src="{{secure_asset('js/owl.carousel.min.js')}}"></script>
+<script src="{{secure_asset('js/jquery.sticky.js')}}"></script>
+<script src="{{secure_asset('js/jquery.waypoints.min.js')}}"></script>
+<script src="{{secure_asset('js/jquery.animateNumber.min.js')}}"></script>
+<script src="{{secure_asset('js/jquery.fancybox.min.js')}}"></script>
+<script src="{{secure_asset('js/jquery.easing.1.3.js')}}"></script>
+<script src="{{secure_asset('js/aos.js')}}"></script>
 
-<script src="{{asset('js/main.js')}}"></script>
+<script src="{{secure_asset('js/main.js')}}"></script>
 
 </body>
 </html>
