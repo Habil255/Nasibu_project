56b95a6e3705b0b4585870c49521eb3a91368451
Habil <raphelhabil09@gmail.com>
2021-06-24 13:12:56 +0300

    title changes

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <title>y4c Innovation Hub&mdash;</title>
+  <title>y4c Innovation Hub</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
